9699512b32851175a939b86f102e2b27992c774a
tiagobalan2 <tiagobalan@edu.unifil.br>
2024-03-11 16:18:16 -0300

    atualizando

--- src/main/java/org/example/ServicoAnimal.java
+++ src/main/java/org/example/ServicoAnimal.java
@@ -15,7 +15,7 @@ public class ServicoAnimal {
         if (animal.getNomeDono() == null || animal.getNomeDono().trim().isEmpty()) {
             return 0;
         }
-        if(animal.getNomeAnimal() == null || animal.getNomeAnimal().trim().isEmpty(){
+        if(animal.getNomeAnimal() == null || animal.getNomeAnimal().trim().isEmpty()){
             return 0;
         }
         return 1;
@@ -34,7 +34,56 @@ public class ServicoAnimal {
     //edicao de animal
     public static boolean editarAnimal(Animal animal) {
         try {
-
+            if (validarAnimal(animal) == 1) {
+                //edicao animal
+                for (Animal a : listaAnimal) {
+                    if(a.getIdAnimal() == animal.getIdAnimal()) {
+                        a.setNomeDono(animal.getNomeDono());
+                        a.setNomeAnimal(animal.getNomeAnimal());
+                        a.setIdadeAnimal(animal.getIdadeAnimal());
+                        return true;
+                    }
+                }
+                return false;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
         }
     }
+
+    //exclusao de animal
+    public static boolean excluirAnimal(int idAnimal){
+        for (int i = 0; i < listaAnimal.size(); i++) {
+            if(listaAnimal.get(i).getIdAnimal() == idAnimal) {
+                listaAnimal.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //consultar animal por id
+    public static Animal consultarAnimal(int idAnimal) {
+        Animal animal = null;
+        for (Animal a: listaAnimal) {
+            if(a.getIdAnimal() == idAnimal) {
+                animal = a;
+                break;
+            }
+        } return animal;
+    }
+
+    public static List<Animal> getListaAnimal() {
+        return listaAnimal;
+    }
+
+    public static void setListaAnimal(List<Animal> listaAnimal) {
+
+        ServicoAnimal.listaAnimal = listaAnimal;
+    }
+
+
+
 }
